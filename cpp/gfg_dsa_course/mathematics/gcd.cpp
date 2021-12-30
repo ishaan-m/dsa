9d018b74bd1045a76ba617de012f8209a6d675fd
ishaan-m <ishaanmanudhane1@gmail.com>
2021-12-30 17:12:13 +0530

    gfg mathematics v5 gcd/lcm

--- cpp/gfg_dsa_course/mathematics/gcd.cpp
+++ cpp/gfg_dsa_course/mathematics/gcd.cpp
@@ -3,16 +3,25 @@ using namespace std;
 
 //gcd(a,b)==gcd(a,a-b)
 
+/*
+gcd(5,15)->gcd(15,5%15)->gcd(5,15%5)->return 5;
+gcd(5,7)->gcd(7,5%7)->gcd(5,7%5)->gcd(2,5%2)->gcd(1,2%1)->return 1;
+
+*/
 int gcd(int a,int b){
-    while(a!=b){
-      if(a<b){
-        b=b-a;
-      }
-      else{
-        a=a-b;
-      }
+  //forcing a to be the bigger number b to be the smaller number
+    if(b==0){
+      //if smaller number is perfectly dividing larger number i.e. b is zero (a%b in previous func call), it is the gcd, so return it
+      return a;
     }
-    return a;
+    return gcd(b,a%b);
+}
+
+//lcm(a,b)== a*b/gcd(a,b)
+
+int lcm(int a,int b){
+  //formula return
+  return (a*b)/(gcd(a,b));
 }
 
 int main(){
@@ -20,8 +29,7 @@ int main(){
   int a,b;
   cin>>a>>b;
   cout<<gcd(a,b)<<"\n";
-
-
+  cout<<lcm(a,b)<<"\n";
 
   return 0;
 }
