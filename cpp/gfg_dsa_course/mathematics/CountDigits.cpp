2bc708588294e9a8409253ddfe3913d7d53599c0
ishaan-m <ishaanmanudhane1@gmail.com>
2021-12-30 14:33:51 +0530

    gfg maths v1

---
+++ cpp/gfg_dsa_course/mathematics/CountDigits.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int counter(int n){
+  int result=0;
+  while(n){
+    result++;
+    n/=10;
+  }
+  
+  return result;
+}
+
+int main(){
+
+  int n;
+  cin>>n;
+
+  cout<<counter(n);
+
+
+  return 0;
+}
