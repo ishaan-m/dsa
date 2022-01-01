cdf18700da1d94ab0f30c66bb9a1a094713db715
ishaan-m <ishaanmanudhane1@gmail.com>
2022-01-01 16:34:42 +0530

    gfg mathematics v9 Sieve

---
+++ cpp/gfg_dsa_course/mathematics/Sieve.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void sieveGenerator(vector<bool> &sieve){
+  int n=sieve.size()-1;
+
+  for(int i=2;i*i<=n;i++){
+      if(sieve[i]){
+        for(int j=i*i;j<=n;j+=i){
+          sieve[j]=false;
+        }
+      }
+  }
+  
+  sieve[0]=false;
+  sieve[1]=false;
+  return;
+}
+
+int main(){
+
+
+  int n;
+  cin>>n;
+  vector<bool> sieve(n+1,true);
+
+  sieveGenerator(sieve);
+
+  for(int i=2;i<sieve.size();i++){
+    cout<<sieve[i]<<" ";
+  }
+  cout<<"\n";
+
+
+  return 0;
+}
