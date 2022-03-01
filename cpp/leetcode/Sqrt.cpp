051b7f33404474b12d295510b85290e9bfdf0050
ishaan-m <ishaanmanudhane1@gmail.com>
2022-03-01 14:55:10 +0530

    hi

---
+++ cpp/leetcode/Sqrt.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int mySqrt(int x){
+	int result=-1;
+	for(int i=0;i*i<=x;i++){
+		result=i;
+	}
+
+
+	return result;
+}
+int main(){
+	int x;
+	cin>>x;
+	cout<<mySqrt(x);
+	return 0;
+}
